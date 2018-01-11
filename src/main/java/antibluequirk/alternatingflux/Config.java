9e964b18e413ca3cc0ab4023eca39edb23b8f07e
AntiBlueQuirk <AntiBlueQuirk@users.noreply.github.com>
2018-01-11 00:05:16 -0600

    Rebalancing due to miscalculations
    The IE config file says that the loss ratio for wire is applied every 16 blocks,
    but this is incorrect. According to the code, the penalty is actually applied
    every X blocks, where X is the maximum length of that tier of wire.
    
    Long story short, the loss ratio I selected was based on the incorrect loss calculations,
    so I've done a rebalance to match the correct calculations.

--- src/main/java/antibluequirk/alternatingflux/Config.java
+++ src/main/java/antibluequirk/alternatingflux/Config.java
@@ -24,16 +24,16 @@ public class Config {
 		@Comment({ "The transfer rates for the wires.", "Order: AF" })
 		public static int[] wireTransferRate = new int[] { 131072 };
 
-		@Comment({ "The percentage of power lost every 16 blocks of distance for a wire network.",
-		"The default value of 0.00125 for AF is approximately equivalent to 8% over 1024 blocks, though in practice this will be closer to 11.2% due to the capacity penalty.",
-		"If you'd like to halve that penalty to 4% (5.6%), set this value to 0.000625. (Or consider putting in some repeaters in your line. :) )",
+		@Comment({ "The percentage of power lost every X blocks of distance for a wire network. (X is equal to the maximum wire length.)",
+		"The default value of 0.005 for AF is approximately equivalent to 10.67-14.93% over 1024 blocks, depending on the capacity penalty.",
+		"If you'd like to halve that penalty to 5.33-7.47%, set this value to 0.0025.",
 		"Order: AF" })
-		public static double[] wireLossRatio = new double[] { 0.00125 };
+		public static double[] wireLossRatio = new double[] { 0.005 };
 
 		@Comment({ "The RGB color of the wires.", "Order: AF" })
 		public static int[] wireColouration = new int[] { 0xf6866c };
 
-		@Comment({ "The maximum length of a single wire.", "Order: AF" })
+		@Comment({ "The maximum length of a single wire. The loss penalty is applied for each of these lengths, so increasing this value increases the efficiency of the wire.", "Order: AF" })
 		public static int[] wireLength = { 48 };
 
 	}
@@ -49,7 +49,7 @@ public class Config {
 		AFWireType.wire_length = AFConfig.wireLength;
     
 		alternatingflux_afTransferRate = AFConfig.wireTransferRate[0];
-		alternatingflux_afkmLossMin = ((1024/16)*AFConfig.wireLossRatio[0]*100);
+		alternatingflux_afkmLossMin = ((1024/AFConfig.wireLength[0])*AFConfig.wireLossRatio[0]*100);
 		alternatingflux_afkmLossMax = alternatingflux_afkmLossMin*1.4;
 		//Crude rounding, because we can't control the formatting.
 		alternatingflux_afkmLossMin = Math.round(alternatingflux_afkmLossMin * 10.0) / 10.0;
